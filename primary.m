6fc7dd833f66152ec093face40a43ba384924c8d
Lucas Unnerfelt <lucasunnerfelt@gmail.com>
2019-08-14 17:28:08 +0200

    Added instructions to primary.m

--- primary.m
+++ primary.m
@@ -1,4 +1,12 @@
 function G = primary(n, Z, Z0, EoED, T)
+% n, Z, Z0 are column vectors with same length as the number of species.
+% n contains the densities (m^-3).
+% Z contains the atomic numbers.
+% Z0 contains the charges.
+% EoED is the electric field normalized to the Dreicer field.
+% T is the temperature in ev.
+% When the inputs are matrices, the function will be applied to each column
+% seperately.
 
 nfree = dot(n, Z0);
 ntot = dot(n, Z);
